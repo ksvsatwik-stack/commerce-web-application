48e4b81ceae2cfb5cec36b1528f96c95663012b3
ksvsatwik-stack <ksvsatwik06@gmail.com>
2026-08-05 19:10:13 +0530

    users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-const users = [
-  { username: "admin", password: "admin123", role: "Admin" },
-  { username: "user", password: "user123", role: "User" }
-];
+router.post("/register", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-router.get('/', (req, res) => {
-  res.json(users);
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword
+  });
+
+  await user.save();
+  res.json({ message: "User Registered" });
 });
 
 module.exports = router;
